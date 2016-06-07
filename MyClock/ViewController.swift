e56fe1d37fde6185a2c9716c2926ba97d7b1159e
ChengFei <2269192760@qq.com>
2016-06-07 15:15:11 +0800

    clock implemented by core animation
    clock implemented by core animation

--- MyClock/ViewController.swift
+++ MyClock/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var hourHand: UIImageView!
+    @IBOutlet weak var minuteHand: UIImageView!
+    @IBOutlet weak var secondHand: UIImageView!
+    var timer:NSTimer = NSTimer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        hourHand.layer.anchorPoint = CGPointMake(0.5, 0.7)
+        minuteHand.layer.anchorPoint = CGPointMake(0.5, 0.7)
+        secondHand.layer.anchorPoint = CGPointMake(0.5, 0.7)
+
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: #selector(ViewController.tick), userInfo: nil, repeats: true)
+        
+        //初始化 ,第一秒开始就跑起来
+        self.tick()
+        
+    }
+    
+    func tick() {
+    //convert timer to hour minute and second
+        let calender = NSCalendar.init(calendarIdentifier: NSCalendarIdentifierGregorian)
+        let currentDate = NSDate()
+        let hourComponents = calender!.components(NSCalendarUnit.Hour , fromDate: currentDate)
+        let minuteComponents = calender!.components(NSCalendarUnit.Minute , fromDate: currentDate)
+        let secondComponents:NSDateComponents = calender!.components(NSCalendarUnit.Second , fromDate: currentDate)
+        
+        
+        let hoursAngle:Double = Double(Double(hourComponents.hour) / 12.0) * (M_PI * 2.0)
+        let minutesAngle:Double = Double(Double(minuteComponents.minute) / 60.0) * (M_PI * 2.0)
+        let secondsAngele:Double = Double(Double(secondComponents.second) / 60.0) * (M_PI * 2.0)
+        print("\(hourComponents.hour):\(minuteComponents.minute):\(secondComponents.second)")
+        hourHand.transform = CGAffineTransformMakeRotation(CGFloat(hoursAngle))
+        minuteHand.transform = CGAffineTransformMakeRotation(CGFloat(minutesAngle))
+        secondHand.transform = CGAffineTransformMakeRotation(CGFloat(secondsAngele))
     }
 
     override func didReceiveMemoryWarning() {
